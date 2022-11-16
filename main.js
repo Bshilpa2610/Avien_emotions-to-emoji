1714c67398b09bf8fadefeb4d135c0b6e97d93c2
Bshilpa2610 <45521000+Bshilpa2610@users.noreply.github.com>
2022-11-16 16:28:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+guess1="";
+guess2="";
+
+
+/*Version Log*/
+
+console.log('ml5 Version:', ml5.version)
+
+/*Camera Setting*/
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90    
+})
+camera = document.getElementById('camera')
+Webcam.attach('#camera')
+
+
+function snapLens(){
+    Webcam.snap(function(data_uri){
+        document.getElementById('result').innerHTML = '<img id= "captured_image" src = "'+data_uri+'"/>';
+    });
+
+   
+}
+
+/*
+function setUp(){
+    setTimeout(function(){
+    snapLens()
+    }  , 5000)
+}
+*/
+
+
+
+/*Sorter Setting*/
+sorter = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8JTRmLOu6/model.json', modelReady)
+function modelReady(){
+    console.log('The model is ready!')
+}
+
+
+function check(){
+    img = document.getElementById('captured_image');
+    sorter.classify(img, gotResult);
+}
+function gotResult(error , results){
+    if (error){
+        console.error(error);
+        document.getElementById('poseName').innerHTML = results[0].label;
+        document.getElementById('poseName2').innerHTML = results[1].label;
+    } else {
+        console.log(results);
+        document.getElementById('poseName').innerHTML = results[0].label;
+        document.getElementById('poseName2').innerHTML = results[1].label;
+        guess1 = results[0].label
+        guess2 = results[1].label
+        speak();
+    }
+}
+
+/*Speak Results*/
+function speak(){
+    var synth = window.speechSynthesis;
+    text1 = 'My first guess is ' + guess1;
+    text2 = 'My first guess is ' + guess2;
+    var speech = new SpeechSynthesisUtterance(text1 + text2);
+    synth.speak(speech);
+}
